c6817a0de52180b8ebf2caa6586059c4cc779775
8y8Si <tverduzco48@gmail.com>
2025-07-24 04:50:11 +0000

    Filtra propiedades publicadas en index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a1a411029d045afd"; }
+/******/ 		__webpack_require__.h = function() { return "4b353e9254105650"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
